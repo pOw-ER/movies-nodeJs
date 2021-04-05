1a2965539a7ea8bc3ab9ef4e84c5b939708abd67
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-04-05 15:56:33 +0200

    params

--- main.js
+++ main.js
@@ -8,22 +8,41 @@ app.listen(3002, () => {
   console.log('listening');
 })
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'))
 })
-app.get('/movies', (req, res) => {
-  res.send(movies.map(movie =>
-    `
-    <div class="movie-card">
-      <a href="">
-        <h1>${movie.title}</h1>
-        <p>${movie.year}</p>
-        <p>${movie.director}</p>
-        <p>${movie.duration}</p>
-        <p>${movie.genre}</p>
-        <p>${movie.rate}</p>
-      </a>
-    </div>`
-  ).join('')
+app.get('/movies/api', (req, res) => {
+  res.send(
+    movies.map(movie =>
+      `
+    <div class= "movie-card" >
+      <h1>${movie.title}</h1>
+      <p>${movie.year}</p>
+      <p>${movie.director}</p>
+      <p>${movie.duration}</p>
+      <p>${movie.genre}</p>
+      <p>${movie.rate}</p>
+    </div> `
+    ).join('')
+  )
+})
+app.get('/movies/api/:year', (req, res) => {
+  // console.log(req.params.year);
+  let movie = movies.filter(movie => movie.year === req.params.year)
+  // console.log(movie);
+  res.send(
+    movie.map(movie =>
+      `
+    <div class= "movie-card" >
+      <h1>${movie.title}</h1>
+      <p>${movie.year}</p>
+      <p>${movie.director}</p>
+      <p>${movie.duration}</p>
+      <p>${movie.genre}</p>
+      <p>${movie.rate}</p>
+    </div> `
+    ).join('')
   )
 })
